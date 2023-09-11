3b29c6567bbe1ae1db6e32af320af7b53bf08832
azrimahanif <azrimahanif@gmail.com>
2023-09-11 21:43:12 +0800

    update html render method

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require("express");
 const home = require('./controller/get/homeController');
-const user = require('./controller/get/userController');
 // create server
 const app = express();
 const PORT = 3000;
 
-app.set("view engine", "ejs");
-
-// app.get("/", (req, res) => {
-//   // Data to pass to the template (replace with your own data)
-//   res.sendFile(__dirname + "/views/index.html");
-// });
 
 
 app.use(home)
